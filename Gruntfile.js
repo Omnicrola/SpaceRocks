6a062606ab041033dd95fc5bb720211913c2f1b2
Eric Schreffler <omnicrola@gmail.com>
2015-03-18 21:34:37 -0400

    added package.json and gruntfile for building

---
+++ Gruntfile.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Eric on 3/18/2015.
+ */
---
+++ bin/spacerocks.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Eric on 3/14/2015.
+ */
+(function () {
+    'use strict';
+    var spaceRocks  = {};
+    spaceRocks.main = function () {
+    };
+})();
+
